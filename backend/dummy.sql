f3172632d9bf70a368a4ced5e7f16ea6add72b35
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-03-15 21:10:02 +0000

    add configuration for web module

---
+++ backend/dummy.sql
@@ -0,0 +1,14 @@
+insert into todo
+values (1, null, 'some basic description', false, false, 'first test todo', null);
+
+insert into todo
+values (2, null, 'some basic description', false, false, 'second test todo', null);
+
+insert into todo
+values (3, null, 'some basic description', false, false, 'third test todo', null);
+
+insert into todo
+values (4, null, 'some basic description', false, false, 'fourth test todo', null);
+
+insert into todo
+values (5, null, 'some basic description', false, false, 'fifth test todo', null);
