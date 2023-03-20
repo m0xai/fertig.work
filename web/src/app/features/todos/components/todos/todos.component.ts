26a3eb04944e7826823fe0c0c8bdda2a0291beb3
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-03-21 00:19:10 +0100

    feat(web): init todos component with working fetch API request

--- web/src/app/features/todos/components/todos/todos.component.ts
+++ web/src/app/features/todos/components/todos/todos.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoModel } from '../../models/todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
 })
-export class TodosComponent {}
+export class TodosComponent implements OnInit {
+  constructor(private todoService: TodoService) {}
+
+  todos: TodoModel[] = [];
+
+  ngOnInit() {
+    this.getTodos();
+  }
+
+  getTodos() {
+    this.todoService.fetch().subscribe((todos: TodoModel[]) => {
+      this.todos = [...todos];
+    });
+  }
+}
