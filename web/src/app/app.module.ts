8572cecab7843972167943973c8bddd077cd6000
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-03-21 22:24:54 +0100

    chore(web): add angular-material package into project

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosModule } from './features/todos/todos.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, TodosModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, TodosModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
