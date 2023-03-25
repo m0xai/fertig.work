f68f15751b926773d37757c087450af5ce87fb6d
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-03-25 23:00:10 +0100

    feat(backend): update backend to add custom user entity

--- backend/src/main/java/work/fertig/backend/user/User.java
+++ backend/src/main/java/work/fertig/backend/user/User.java
@@ -5,50 +5,78 @@ import java.util.Collection;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "fw_user") // table user is for postgres reserved
 public class User implements UserDetails {
-        private String username;
-        private String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-        @Override
-        public Collection<? extends GrantedAuthority> getAuthorities() {
-                // TODO Auto-generated method stub
-                return null;
-        }
+    @Column(nullable = false, unique = true)
+    private String username;
 
-        @Override
-        public String getPassword() {
-                // TODO Auto-generated method stub
-                return password;
-        }
+    @Column(nullable = false)
+    private String password;
 
-        @Override
-        public String getUsername() {
-                // TODO Auto-generated method stub
-                return username;
-        }
+    @Column(nullable = false, unique = true)
+    private String email;
 
-        @Override
-        public boolean isAccountNonExpired() {
-                // TODO Auto-generated method stub
-                return false;
-        }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
 
-        @Override
-        public boolean isAccountNonLocked() {
-                // TODO Auto-generated method stub
-                return false;
-        }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-        @Override
-        public boolean isCredentialsNonExpired() {
-                // TODO Auto-generated method stub
-                return false;
-        }
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
-        @Override
-        public boolean isEnabled() {
-                // TODO Auto-generated method stub
-                return false;
-        }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 
 }
