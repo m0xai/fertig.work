181f5f86c333a52a3c9b4aaf1769a4bcae7d56a2
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-03-25 21:08:20 +0100

    feat(web): create auth module

---
+++ web/src/app/features/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AuthService } from './auth.service';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, BrowserModule, FormsModule],
+  providers: [AuthService],
+})
+export class AuthModule {}
