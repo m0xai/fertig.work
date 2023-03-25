54d31e9569682c7d908f247aeecfab194aed36c3
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-03-25 23:01:46 +0100

    feat(backend): add user repository with existsByEmail custom function

---
+++ backend/src/main/java/work/fertig/backend/user/UserRepository.java
@@ -0,0 +1,11 @@
+package work.fertig.backend.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String mail);
+
+    boolean existsByEmail(String mail);
+}
