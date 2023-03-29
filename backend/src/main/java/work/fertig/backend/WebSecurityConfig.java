185d8a953f47ef39001141c5fc5a00812a8e7d8c
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-03-29 21:33:37 +0200

    style(backend): reformat java code

--- backend/src/main/java/work/fertig/backend/WebSecurityConfig.java
+++ backend/src/main/java/work/fertig/backend/WebSecurityConfig.java
@@ -1,36 +1,51 @@
 package work.fertig.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import work.fertig.backend.user.FWUserDetailsService;
+import work.fertig.backend.user.FWUserRepository;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.authorizeHttpRequests((requests) -> requests
-        .requestMatchers("/**").permitAll()
-        .anyRequest().authenticated());
-    httpSecurity.csrf().disable();
 
-    return httpSecurity.build();
-  }
+    @Autowired
+    private FWUserRepository userRepository;
 
-  @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails user = User.withDefaultPasswordEncoder()
-        .username("user")
-        .password("password")
-        .roles("USER")
-        .build();
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-    return new InMemoryUserDetailsManager(user);
-  }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new FWUserDetailsService(userRepository);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests().requestMatchers("/**").authenticated();
+        http.httpBasic();
+        http.formLogin();
+
+        return http.build();
+    }
+
 }
--- backend/src/main/java/work/fertig/backend/todo/Todo.java
+++ backend/src/main/java/work/fertig/backend/todo/Todo.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -15,105 +16,106 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "todo")
+@CrossOrigin(origins = "http://localhost:4200")
 public class Todo {
-  @Column(name = "id")
-  private @Id @GeneratedValue(strategy = GenerationType.AUTO) Long id;
+    @Column(name = "id")
+    private @Id @GeneratedValue(strategy = GenerationType.AUTO) Long id;
 
-  @Column(name = "name", nullable = false)
-  private String name;
-  @Column(name = "description", nullable = false)
-  private String description;
-  @Column(name = "is_done")
-  private Boolean isDone;
-  @Column(name = "is_draft")
-  private Boolean isDraft;
-  @Column(name = "created_at")
-  @CreationTimestamp
-  private Timestamp createdAt;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "is_done")
+    private Boolean isDone;
+    @Column(name = "is_draft")
+    private Boolean isDraft;
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private Timestamp createdAt;
 
-  @Column(name = "updated_at")
-  @UpdateTimestamp
-  private Timestamp updatedAt;
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 
-  public Todo() {
-  }
-
-  public Todo(String name, String description) {
-    this.name = name;
-    this.description = description;
-    this.isDone = false;
-    this.isDraft = false;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public Boolean getIsDone() {
-    return isDone;
-  }
-
-  public void setIsDone(Boolean isDone) {
-    this.isDone = isDone;
-  }
-
-  public Boolean getIsDraft() {
-    return isDraft;
-  }
-
-  public void setIsDraft(Boolean isDraft) {
-    this.isDraft = isDraft;
-  }
-
-  public Timestamp getCreatedAt() {
-    return createdAt;
-  }
-
-  public void setCreatedAt(Timestamp createdAt) {
-    this.createdAt = createdAt;
-  }
-
-  public Timestamp getUpdatedAt() {
-    return updatedAt;
-  }
-
-  public void setUpdatedAt(Timestamp updatedAt) {
-    this.updatedAt = updatedAt;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
+    public Todo() {
     }
 
-    if (!(obj instanceof Todo)) {
-      return false;
+    public Todo(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.isDone = false;
+        this.isDraft = false;
     }
 
-    Todo tmp = (Todo) obj;
-    return Objects.equals(this.name, tmp.name) && Objects.equals(this.description, tmp.description)
-        && Objects.equals(this.isDone, tmp.isDone) && Objects.equals(this.isDraft, tmp.isDraft);
-  }
+    public Long getId() {
+        return id;
+    }
 
-  @Override
-  public String toString() {
-    return "Todo {id=" + this.id + ", name=" + this.name + ")";
-  }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getIsDone() {
+        return isDone;
+    }
+
+    public void setIsDone(Boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    public Boolean getIsDraft() {
+        return isDraft;
+    }
+
+    public void setIsDraft(Boolean isDraft) {
+        this.isDraft = isDraft;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof Todo)) {
+            return false;
+        }
+
+        Todo tmp = (Todo) obj;
+        return Objects.equals(this.name, tmp.name) && Objects.equals(this.description, tmp.description)
+                && Objects.equals(this.isDone, tmp.isDone) && Objects.equals(this.isDraft, tmp.isDraft);
+    }
+
+    @Override
+    public String toString() {
+        return "Todo {id=" + this.id + ", name=" + this.name + ")";
+    }
 }
