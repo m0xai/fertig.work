336718674d27b522327fb8162b5ef420090defb8
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-04-07 16:25:29 +0200

    refactor(backend): split FWUser and FWUserDetailsSerivice

--- backend/src/main/java/work/fertig/backend/user/FWUser.java
+++ backend/src/main/java/work/fertig/backend/user/FWUser.java
@@ -1,22 +1,32 @@
 package work.fertig.backend.user;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
-@Table(name = "fw_user") // table user is for postgres reserved
-public class FWUser implements UserDetails {
+@Table(name = "fw_users") // table user is for postgres reserved
+public class FWUser {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -34,6 +44,21 @@ public class FWUser implements UserDetails {
     @Email(message = "Email should be a valid address.")
     private String email;
 
+    @ManyToMany
+    @JoinTable(name = "fw_users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles;
+
+    public FWUser() {
+    };
+
+    public FWUser(@NotBlank(message = "Username field can't leave blank.") String username,
+            @NotBlank(message = "Password field can't leave blank.") String password,
+            @NotBlank(message = "This  field can't leave blank.") @Email(message = "Email should be a valid address.") String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
     public Long getId() {
         return this.id;
     }
@@ -43,7 +68,7 @@ public class FWUser implements UserDetails {
     }
 
     public String getPassword() {
-        return password;
+        return this.password;
     }
 
     public void setUsername(String username) {
@@ -62,33 +87,12 @@ public class FWUser implements UserDetails {
         this.email = email;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+    public Collection<Role> getRoles() {
+        return this.roles;
     }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonExpired'");
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
     }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonLocked'");
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isCredentialsNonExpired'");
-    }
-
-    @Override
-    public boolean isEnabled() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
-    }
 }
--- backend/src/main/java/work/fertig/backend/user/FWUserDetailsService.java
+++ backend/src/main/java/work/fertig/backend/user/FWUserDetailsService.java
@@ -3,24 +3,29 @@ package work.fertig.backend.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import work.fertig.backend.user.exceptions.UserNotFoundException;
 
 @Service
 public class FWUserDetailsService implements UserDetailsService {
 
     @Autowired
-    private FWUserRepository userRepository;
+    private final FWUserRepository userRepository;
+
+    public FWUserDetailsService(FWUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        FWUser user = userRepository.findByUsername(username);
-        if (user == null) {
+    public UserDetails loadUserByUsername(String username) throws UserNotFoundException {
+        FWUser fwUser = userRepository.findByUsername(username);
+        if (fwUser == null) {
             // TODO: Create custom user not found exception.
-            throw new RuntimeException("User not found");
+            throw new UserNotFoundException("User with the username: " + username + " not found");
         }
 
-        return user;
+        return new FWUserDetails(fwUser);
     }
 
 }
