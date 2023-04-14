d4b6bebfe2ab33bfbe989028a2cde2e6b47177f6
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-04-14 20:35:40 +0200

    feat(web): add login and home component into router

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/components/login/login.component';
+import { HomeComponent } from './features/home/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: 'home',
+  // },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
