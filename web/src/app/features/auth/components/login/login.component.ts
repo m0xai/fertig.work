59e268ea21a0c5c622ba313d56fa2a99ce6f17ce
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-04-14 20:22:32 +0200

    feat(web): init basic login component

---
+++ web/src/app/features/auth/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  credentials = { username: '', password: '' };
+
+  login() {
+    this.authService.authenticate(this.credentials, () => {
+      this.router.navigateByUrl('/home');
+    });
+  }
+}
