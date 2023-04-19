fd6348fcc2db6fdfb1cab3caa02f8d752071b4aa
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-04-19 23:42:59 +0200

    feat(backend): init jwtAuthSerivce class

---
+++ backend/src/main/java/work/fertig/backend/auth/JwtAuthService.java
@@ -0,0 +1,7 @@
+package work.fertig.backend.auth;
+
+public class JwtAuthService {
+    public static final String SECRET = "lowkey";
+    public static final Long EXPIRATION_TIME = (long) 86400000;
+
+}
--- web/src/app/shared/resource.service.ts
+++ web/src/app/shared/resource.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { ResourceModel } from './ResourceModel';
@@ -20,8 +20,15 @@ export abstract class ResourceService<T extends ResourceModel<T>> {
   }
 
   public fetch(): Observable<T[]> {
+    //? Headers only set for other requests, that require authentication
+    // TODO: Set auth credentials to the localStorage or something elsewhere to use in Resource Service
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Basic ${btoa('derya:1299')}`
+    );
+    //   .set('withCredentials', 'true');
     return this.httpClient
-      .get<T[]>(`${this.apiURL}`)
+      .get<T[]>(`${this.apiURL}`, { headers })
       .pipe(map((result) => result.map((i) => new this.tConstructor(i))));
   }
 
