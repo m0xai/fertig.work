80ccd637aa0725b7443e77ab033ab64de4bf79cf
m0xai <keremzopcuk@gmail.com>
2023-07-18 23:37:09 +0200

    refactor(backend): Move validation logic from Task to TaskDTORequest class

--- backend/src/main/java/work/fertig/backend/task/dtos/TaskDTORequest.java
+++ backend/src/main/java/work/fertig/backend/task/dtos/TaskDTORequest.java
@@ -1,23 +1,33 @@
 package work.fertig.backend.task.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import work.fertig.backend.task.Task;
+import work.fertig.backend.user.FWUser;
+import work.fertig.backend.user.exceptions.UserNotFoundException;
 
 import java.sql.Timestamp;
 
 @Data
 @AllArgsConstructor
 @Builder
+@Component
 public class TaskDTORequest implements TaskDTO {
     private Long id;
+    @NotBlank(message = "A name for the task is required.")
     private String name;
+    @NotBlank(message = "A description for the task is required.")
     private String description;
     private Boolean isDone;
     private Boolean isDraft;
     private Timestamp createdAt;
     private Timestamp updatedAt;
+    @NotNull(message = "User id cannot be blank.")
+    private Long createdById;
 
     private TaskDTORequest() {
     }
