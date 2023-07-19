386dedd27208b8dd837a261257fa8f2434fde97f
m0xai <keremzopcuk@gmail.com>
2023-07-19 14:19:11 +0200

    refactor(backend): move createUser logic into user service

--- backend/src/main/java/work/fertig/backend/user/FWUserController.java
+++ backend/src/main/java/work/fertig/backend/user/FWUserController.java
@@ -1,26 +1,23 @@
 package work.fertig.backend.user;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.RequestEntity.BodyBuilder;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import jakarta.validation.Valid;
 import work.fertig.backend.auth.JwtAuthService;
-import work.fertig.backend.user.exceptions.UserAlreadyExistsException;
+import work.fertig.backend.user.dtos.FWUserDTORequest;
+import work.fertig.backend.user.dtos.FWUserDTOResponse;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -30,14 +27,15 @@ public class FWUserController {
     @Autowired
     private final FWUserRepository repository;
 
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
     private AuthenticationManager authenticationManager;
 
     @Autowired
     private JwtAuthService jwtAuthService;
+
+    @Autowired
+    private FWUserService fwUserService;
 
     public FWUserController(FWUserRepository repository) {
         this.repository = repository;
@@ -56,30 +54,15 @@ public class FWUserController {
 
     @PostMapping("/register")
     @CrossOrigin(origins = "http://localhost:4200")
-    public ResponseEntity<FWUserDto> createUser(@Valid @RequestBody FWUser user) {
-        if (repository.existsByEmail(user.getEmail())) {
-            throw new UserAlreadyExistsException("email: " + user.getEmail());
-        }
-        if (repository.existsByUsername(user.getUsername())) {
-            throw new UserAlreadyExistsException("username: " + user.getUsername());
-        }
-
-        FWUser fwUser = new FWUser();
-        fwUser.setUsername(user.getUsername());
-        fwUser.setEmail(user.getEmail());
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        fwUser.setPassword(encodedPassword);
-        FWUser savedUser = repository.save(user);
-
-        FWUserDto fwUserDto = new FWUserDto(savedUser.getId(), savedUser.getUsername(), savedUser.getPassword(),
-                savedUser.getEmail());
-
-        return new ResponseEntity<>(fwUserDto, HttpStatus.CREATED);
+    public ResponseEntity<FWUserDTOResponse> createUser(@Valid @RequestBody FWUserDTORequest request) {
+        FWUserDTOResponse fwUserDTOResponse = fwUserService.create(request);
+        return new ResponseEntity<>(fwUserDTOResponse, HttpStatus.CREATED);
     }
 
+    // TODO: Move this also in FWUserService
     @PostMapping("/login")
     @CrossOrigin
-    public ResponseEntity<Map> authenticateUser(@RequestBody FWUserDto loginDto) {
+    public ResponseEntity<Map> authenticateUser(@RequestBody FWUserDTORequest loginDto) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 loginDto.getUsername(), loginDto.getPassword()));
 
