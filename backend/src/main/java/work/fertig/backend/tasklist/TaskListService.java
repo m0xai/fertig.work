1221fca3b85fc82c0df1f2f3fd2a81f84c0794b1
m0xai <keremzopcuk@gmail.com>
2023-07-25 23:03:50 +0200

    feat(backend): create TaskListService for business logic of taskList domain

---
+++ backend/src/main/java/work/fertig/backend/tasklist/TaskListService.java
@@ -0,0 +1,44 @@
+package work.fertig.backend.tasklist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import work.fertig.backend.task.Task;
+import work.fertig.backend.task.TaskNotFoundException;
+import work.fertig.backend.task.TaskRepository;
+import work.fertig.backend.tasklist.dtos.TaskListDTORequest;
+import work.fertig.backend.tasklist.dtos.TaskListDTOResponse;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskListService {
+    @Autowired
+    TaskListRepository taskListRepository;
+    @Autowired
+    TaskRepository taskRepository;
+
+    public TaskListDTOResponse create(TaskListDTORequest request) {
+        TaskList convertedTaskList = request.convertToEntity();
+        System.out.println("Deryamcik");
+        TaskList taskList = taskListRepository.save(convertedTaskList);
+        return TaskListDTOResponse.fromTaskList(taskList);
+    }
+
+    public TaskListDTOResponse get(Long id) {
+        Optional<TaskList> oTaskList = taskListRepository.findById(id);
+        if (oTaskList.isEmpty()) {
+            throw new TaskNotFoundException(id);
+        }
+        return TaskListDTOResponse.fromTaskList(oTaskList.get());
+    }
+
+    private List<Task> getTaskItems(List<Long> ids) {
+        if (ids.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No list of tasks were given.");
+        }
+        return taskRepository.findAllById(ids);
+    }
+}
