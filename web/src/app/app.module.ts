38e3a57fb5fff603d62bd19149f01bf5b5aecbe5
m0xai <keremzopcuk@gmail.com>
2023-08-01 22:49:11 +0200

    refactor(frontend): import shared module into app module

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CoreModule } from './core/core.module';
-import { FeaturesModule } from './features/features.module';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CoreModule} from './core/core.module';
+import {SharedModule} from "./shared/shared.module";
+import {FeaturesModule} from "./features/features.module";
 
 
 @NgModule({
@@ -16,9 +17,11 @@ import { FeaturesModule } from './features/features.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
-    FeaturesModule
+    SharedModule,
+    FeaturesModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
