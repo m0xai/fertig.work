b18e02455403288a91060c8b9cefc6d89c70f048
m0xai <keremzopcuk@gmail.com>
2023-08-01 23:04:20 +0200

    refactor(frontend): use current tasks api route, since we renamed todo with task

--- web/src/app/features/todo/services/todo.service.ts
+++ web/src/app/features/todo/services/todo.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
-import { TodoModel } from '../models/todo';
-import { ResourceService } from 'src/app/shared/resource.service';
+import {TodoModel} from '../models/todo';
+import {ResourceService} from 'src/app/shared/resource.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService extends ResourceService<TodoModel> {
   constructor(private http: HttpClient) {
-    super(http, TodoModel, 'http://127.0.0.1:8080/todos/');
+    super(http, TodoModel, 'http://127.0.0.1:8080/api/v1/tasks/');
   }
 }
