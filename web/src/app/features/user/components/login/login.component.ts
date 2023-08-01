152fda7326d6c73d480787633508b656a86c9f07
m0xai <keremzopcuk@gmail.com>
2023-08-02 00:47:12 +0200

    feat(frontend): rewrite login form with build-in angular form / validation

--- web/src/app/features/user/components/login/login.component.ts
+++ web/src/app/features/user/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from 'src/app/core/services/auth/auth.service';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -11,11 +12,28 @@ export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) {
   }
 
-  credentials = {username: '', password: ''};
+  credentials = new FormGroup({
+    username: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required])
+  });
 
   login() {
-    this.authService.authenticate(this.credentials, () => {
-      this.router.navigateByUrl('/home');
-    });
+
+    if (this.credentials.valid) {
+      this.authService.authenticate({
+        username: this.credentials.value.username,
+        password: this.credentials.value.password
+      }, () => {
+        this.router.navigateByUrl('/home');
+      });
+    }
+
+  }
+
+  getErrorMessage(fieldName: string) {
+    if (this.credentials.get(fieldName)!.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return "Please enter a valid " + fieldName;
   }
 }
