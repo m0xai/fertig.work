0371004a49e62f7015beaf5f84ac397ec1df8b90
m0xai <keremzopcuk@gmail.com>
2023-08-01 22:47:54 +0200

    feat(frontend): create login guard for app views

---
+++ web/src/app/shared/services/login.guard.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from "../../core/services/auth/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+  constructor(private authService: AuthService) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.isAuthenticated();
+  }
+
+}
