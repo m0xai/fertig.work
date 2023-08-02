2c12547f1877d9d07c481f2276838abe757a5f70
m0xai <keremzopcuk@gmail.com>
2023-08-02 18:03:48 +0200

    feat(frontend): create fw-alert component for showing alerts

---
+++ web/src/app/shared/components/fw-alert/fw-alert.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FwAlertComponent } from './fw-alert.component';
+
+describe('FwAlertComponent', () => {
+  let component: FwAlertComponent;
+  let fixture: ComponentFixture<FwAlertComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FwAlertComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FwAlertComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ web/src/app/shared/components/fw-alert/fw-alert.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-fw-alert',
+  templateUrl: './fw-alert.component.html',
+  styleUrls: ['./fw-alert.component.css']
+})
+export class FwAlertComponent {
+  @Input() alertType: "none" | "warn" | "error" | "info" = "none"
+  @Input() alertMessage: string = ''
+
+  public hideCard() {
+    console.log("Trying to hid")
+  }
+}
