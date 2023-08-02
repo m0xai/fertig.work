3bcd673b1492a93279a637a5527db9bd6d04c572
m0xai <keremzopcuk@gmail.com>
2023-08-02 17:59:58 +0200

    refactor(frontend): rename getAuthorizationToken to getAuthToken method

--- web/src/app/core/services/auth/auth-interceptor.ts
+++ web/src/app/core/services/auth/auth-interceptor.ts
@@ -1,15 +1,16 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { AuthService } from "./auth.service";
+import {HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {AuthService} from "./auth.service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService) {
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     // Get the auth token from the service.
-    const authToken = this.auth.getAuthorizationToken();
+    const authToken = this.auth.getAuthToken();
     // Clone the request and set the Bearer token in the Authorization header.
     const authReq = req.clone({
       setHeaders: {
