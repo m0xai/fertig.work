c69d7d582ee98c9b81faeb26693f65c30f8cd835
m0xai <keremzopcuk@gmail.com>
2023-08-03 13:34:49 +0200

    feat(frontend): create /tasks for the Tasks Veiw and set titles for the routes

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './features/user/components/login/login.component';
 import {HomeComponent} from './features/home/components/home/home.component';
 import {LoginGuard} from "./shared/services/login.guard";
+import {TodosComponent} from "./features/todo/components/todos/todos.component";
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'home', canActivate: [LoginGuard], component: HomeComponent},
+  {path: 'login', component: LoginComponent, data: {title: "Login"}},
+  {path: 'home', canActivate: [LoginGuard], component: HomeComponent, data: {title: "Home"}},
+  {path: 'tasks', canActivate: [LoginGuard], component: TodosComponent, data: {title: "Tasks"}},
 ];
 
 @NgModule({
