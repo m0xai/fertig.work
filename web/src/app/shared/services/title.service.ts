62eae87bb2706fe8111c4792c16d605e8118632c
m0xai <keremzopcuk@gmail.com>
2023-08-03 13:07:51 +0200

    feat(frontend): create Title service for showing title in certein pages

---
+++ web/src/app/shared/services/title.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TitleService {
+  private titleSubject = new Subject<string>();
+  title$ = this.titleSubject.asObservable();
+
+  setTitle(title: string) {
+    this.titleSubject.next(title);
+  }
+}
