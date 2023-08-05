a6ea08282f39722dede974ff8823d3935db11866
m0xai <keremzopcuk@gmail.com>
2023-08-05 23:00:54 +0200

    refactor(backend): use lombok instead of getter and setter in Project entity

--- backend/src/main/java/work/fertig/backend/project/Project.java
+++ backend/src/main/java/work/fertig/backend/project/Project.java
@@ -4,9 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
+import work.fertig.backend.base.BaseEntity;
 
 @Entity
-public class Project {
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Project extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -14,40 +21,5 @@ public class Project {
     private String title;
     private String description;
     private Boolean isArchived;
-
-    public Project() {
-    }
-
-    public Project(String title) {
-        this.title = title;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Boolean getIsArchived() {
-        return isArchived;
-    }
-
-    public void setIsArchived(Boolean isArchived) {
-        this.isArchived = isArchived;
-    }
 
 }
