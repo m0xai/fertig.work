bb11c431524086cd5cb93003c97d92356c09afc1
m0xai <keremzopcuk@gmail.com>
2023-08-06 00:11:04 +0200

    feat(frontend): create project-create component

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from './features/user/components/login/login.component';
-import {HomeComponent} from './features/home/components/home/home.component';
-import {LoginGuard} from "./shared/services/login.guard";
-import {TodosComponent} from "./features/todo/components/todos/todos.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/user/components/login/login.component';
+import { HomeComponent } from './features/home/components/home/home.component';
+import { LoginGuard } from "./shared/services/login.guard";
+import { TasksComponent } from "./features/task/components/tasks/tasks.component";
+import { ProjectCreateComponent } from "./features/project/project-create/project-create.component";
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent, data: {title: "Login"}},
   {path: 'home', canActivate: [LoginGuard], component: HomeComponent, data: {title: "Home"}},
-  {path: 'tasks', canActivate: [LoginGuard], component: TodosComponent, data: {title: "Tasks"}},
+  {path: 'tasks', canActivate: [LoginGuard], component: TasksComponent, data: {title: "Tasks"}},
+  {
+    path: 'projects/create', canActivate: [LoginGuard], component: ProjectCreateComponent, data: {
+      title: "Create Project"
+    }
+  },
 ];
 
 @NgModule({
