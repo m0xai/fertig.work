b12652a0e5a0b2e22b18de533da02e5806e25e24
m0xai <keremzopcuk@gmail.com>
2023-08-06 00:12:12 +0200

    feat(frontend): create project component

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './features/home/components/home/home.component';
 import { LoginGuard } from "./shared/services/login.guard";
 import { TasksComponent } from "./features/task/components/tasks/tasks.component";
 import { ProjectCreateComponent } from "./features/project/project-create/project-create.component";
+import { ProjectsComponent } from "./features/project/projects/projects.component";
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent, data: {title: "Login"}},
   {path: 'home', canActivate: [LoginGuard], component: HomeComponent, data: {title: "Home"}},
   {path: 'tasks', canActivate: [LoginGuard], component: TasksComponent, data: {title: "Tasks"}},
+  {path: 'projects', canActivate: [LoginGuard], component: ProjectsComponent, data: {title: "Projects"}},
   {
     path: 'projects/create', canActivate: [LoginGuard], component: ProjectCreateComponent, data: {
       title: "Create Project"
