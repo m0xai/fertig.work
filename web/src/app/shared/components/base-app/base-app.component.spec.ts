4d6c14c645b71e91bb9925e1c7f76ff6ea342709
m0xai <keremzopcuk@gmail.com>
2023-08-08 01:42:47 +0200

    feat(frontend): create base app view component for /app path.

---
+++ web/src/app/shared/components/base-app/base-app.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BaseAppComponent } from './base-app.component';
+
+describe('BaseAppComponent', () => {
+  let component: BaseAppComponent;
+  let fixture: ComponentFixture<BaseAppComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BaseAppComponent]
+    });
+    fixture = TestBed.createComponent(BaseAppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
