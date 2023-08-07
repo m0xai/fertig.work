0bfd9a5277f36c616c027bb04447ec63814dc73a
m0xai <keremzopcuk@gmail.com>
2023-08-08 01:42:11 +0200

    feat(frontend): add login and logout guard to kepp users in or out of the app

---
+++ web/src/app/shared/services/logout.guard.ts
@@ -0,0 +1,15 @@
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { LoginGuard } from "./login.guard";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogoutGuard implements CanActivate {
+  constructor(private loginGuard: LoginGuard) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    return !this.loginGuard.canActivate(route, state);
+  }
+}
