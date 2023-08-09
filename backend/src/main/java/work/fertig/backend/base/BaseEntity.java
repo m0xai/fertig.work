bbf8407c6383d5a2d3a20d4473eb320e4b1ffe19
m0xai <keremzopcuk@gmail.com>
2023-08-09 22:43:10 +0200

    feat(backend): init ProjectDTO and use builder annotations in BaseDTO

--- backend/src/main/java/work/fertig/backend/base/BaseEntity.java
+++ backend/src/main/java/work/fertig/backend/base/BaseEntity.java
@@ -2,14 +2,20 @@ package work.fertig.backend.base;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
 
 @Data
+@SuperBuilder
 @MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
 public abstract class BaseEntity {
 
     @Column(updatable = false)
---
+++ backend/src/main/java/work/fertig/backend/project/dtos/ProjectDTO.java
@@ -0,0 +1,8 @@
+package work.fertig.backend.project.dtos;
+
+import work.fertig.backend.base.BaseDTO;
+
+public interface ProjectDTO extends BaseDTO {
+    String getTitle();
+
+}
