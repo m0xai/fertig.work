754f3a4ed1a02a7f1003bcd4bc5b43248657216b
m0xai <keremzopcuk@gmail.com>
2023-08-11 01:50:33 +0200

    feat(frontend): make sidenav responsible and add some global css rules

--- web/src/app/shared/components/header/header.component.ts
+++ web/src/app/shared/components/header/header.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from "../../../core/services/auth/auth.service";
 import { TitleService } from "../../services/title.service";
 import { Subscription } from "rxjs";
+import { MatDrawer } from "@angular/material/sidenav";
+import { BreakpointObserver } from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnDestroy {
+export class HeaderComponent implements OnDestroy, OnInit {
+  @ViewChild(MatDrawer, {static: true})
+  drawer!: MatDrawer;
   public title = "";
   public menuItems = [
     {
@@ -69,11 +73,24 @@ export class HeaderComponent implements OnDestroy {
   ]
   private subscription: Subscription;
 
-  constructor(private auth: AuthService, private titleService: TitleService) {
+  constructor(private auth: AuthService, private titleService: TitleService, private observer: BreakpointObserver) {
     this.subscription = this.titleService.title$.subscribe((title) => {
       this.title = title;
     });
   }
+
+  ngOnInit() {
+    this.observer.observe(["(max-width: 800px)"]).subscribe((res) => {
+      if (res.matches) {
+        this.drawer.mode = "over";
+        this.drawer.close();
+      } else {
+        this.drawer.mode = "side";
+        this.drawer.open();
+      }
+    });
+  }
+
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
