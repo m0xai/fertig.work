5f6d97344122aa10624bfd4b01d9e9458c75de9a
m0xai <keremzopcuk@gmail.com>
2023-08-10 12:07:23 +0200

    feat(frontend): create basic register method in authService

--- web/src/app/core/services/auth/auth.service.ts
+++ web/src/app/core/services/auth/auth.service.ts
@@ -5,21 +5,40 @@ import { finalize } from 'rxjs';
 import ICredentials from './models/ICredentials';
 import { CookieService } from 'ngx-cookie-service';
 import { ErrorService } from "../../../shared/services/error.service";
+import { User } from "../../../features/user/models/user";
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient, private cookieService: CookieService, private router: Router, private errorSerivce: ErrorService) {
+  baseUrl = "http://localhost:8080/api/v1/"
+
+  constructor(private http: HttpClient, private cookieService: CookieService, private router: Router, private errorService: ErrorService) {
+  }
+
+  // We don't use resourceService, since we have JWT logic here.
+  register(user: User) {
+    const registerUrl = this.baseUrl + "users"
+    return this.http.post(registerUrl, user, {observe: "response"})
+      .subscribe(
+        (response: HttpResponse<any>) => {
+          if (response.ok) {
+            console.log(response)
+          }
+        }, (error) => {
+          this.errorService.setError(error.error.msg)
+          console.warn(error)
+        }
+      );
   }
 
   authenticate(credentials?: ICredentials, callback?: Function) {
-    const url = 'http://localhost:8080/api/v1/login';
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
     });
+    const loginUrl = this.baseUrl + "login"
 
-    this.http.post(url, credentials, {headers, observe: 'response'})
+    this.http.post(loginUrl, credentials, {headers, observe: 'response'})
       .subscribe(
         (response: HttpResponse<any>) => {
           if (response.ok) {
@@ -29,7 +48,7 @@ export class AuthService {
           return callback?.call(null);
         },
         (error) => {
-          this.errorSerivce.setError(error.error.msg);
+          this.errorService.setError(error.error.msg);
         }
       );
   }
