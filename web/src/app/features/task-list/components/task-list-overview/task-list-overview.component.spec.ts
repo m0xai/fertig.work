1d6d02af6b72834d5888ba83e8865b23d642b10b
m0xai <keremzopcuk@gmail.com>
2023-09-03 10:11:20 +0200

    feat(frontend): create task-list and task-list-overview components

---
+++ web/src/app/features/task-list/components/task-list-overview/task-list-overview.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TaskListOverviewComponent } from './task-list-overview.component';
+
+describe('TaskListOverviewComponent', () => {
+  let component: TaskListOverviewComponent;
+  let fixture: ComponentFixture<TaskListOverviewComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TaskListOverviewComponent]
+    });
+    fixture = TestBed.createComponent(TaskListOverviewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
