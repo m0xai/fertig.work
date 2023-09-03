38deb38344f4a1b3c242b6d87277130e3eb3a6aa
m0xai <keremzopcuk@gmail.com>
2023-09-03 11:40:37 +0200

    refactor(frontend): remove taskList module from features parent module

--- web/src/app/features/features.module.ts
+++ web/src/app/features/features.module.ts
@@ -4,20 +4,18 @@ import { UserModule } from './user/user.module';
 import { ProjectModule } from './project/projects.module';
 import { HomeModule } from './home/home.module';
 import { TaskModule } from './task/task.module';
-import { TaskListModule } from "./task-list/task-list.module";
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    UserModule,
-    HomeModule,
-    ProjectModule,
-    TaskModule,
-    TaskListModule
-  ],
-  exports: [HomeModule, TaskModule, TaskListModule]
+    declarations: [],
+    imports: [
+        CommonModule,
+        UserModule,
+        HomeModule,
+        ProjectModule,
+        TaskModule,
+    ],
+    exports: [HomeModule, TaskModule,]
 })
 export class FeaturesModule {
 }
