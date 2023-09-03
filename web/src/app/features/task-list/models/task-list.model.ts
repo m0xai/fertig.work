9cb41bc234a1464a5c84b5f4280d520d9d610466
m0xai <keremzopcuk@gmail.com>
2023-09-03 10:11:37 +0200

    feat(frontend): create task-list model and service

---
+++ web/src/app/features/task-list/models/task-list.model.ts
@@ -0,0 +1,10 @@
+import { ResourceModel } from "../../../shared/ResourceModel";
+
+export class TaskList extends ResourceModel<TaskList> {
+  public title?: string;
+
+  // tasks omitted here
+  constructor(model?: Partial<TaskList>) {
+    super(model);
+  }
+}
---
+++ web/src/app/features/task-list/services/task-list.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { ResourceService } from "../../../shared/resource.service";
+import { TaskList } from "../models/task-list.model";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskListService extends ResourceService<TaskList> {
+  constructor(private http: HttpClient) {
+    super(http, TaskList, "tasklists/")
+  }
+}
