09f2e98d55d8a4c254e92c32b1464af1659f64a7
m0xai <keremzopcuk@gmail.com>
2023-09-30 22:13:45 +0200

    feat(frontend): introduce builder pattern for task entity

--- web/src/app/features/task/models/task.model.ts
+++ web/src/app/features/task/models/task.model.ts
@@ -4,14 +4,61 @@ export class Task extends ResourceModel<Task> {
   public name?: string;
   public description?: string;
   public isDone?: boolean;
+  public priority?: ETaskPriority;
   public isDraft?: boolean;
   public createdBy?: number;
   public taskList?: number;
+  public status?: ETaskStatus;
 
-  constructor(model?: Partial<Task>) {
-    super(model);
+  public static create() {
+    return new Task();
+  }
+
+  public withName(value: string) {
+    this.name = value;
+    return this;
+  }
+
+  public withDescription(value: string) {
+    this.description = value;
+    return this;
+  }
+
+  public withIsDone(value: boolean) {
+    this.isDone = value;
+    return this;
+  }
+
+  public withIsDraft(value: boolean) {
+    this.isDraft = value;
+    return this;
+  }
+
+  public withCreatedBy(value: number) {
+    this.createdBy = value;
+    return this;
+  }
+
+  public withTaskList(value: number) {
+    this.taskList = value;
+    return this;
+  }
+
+  public withStatus(value: ETaskStatus) {
+    this.status = value;
+    return this;
+  }
+
+  public withPriority(value: ETaskPriority) {
+    this.priority = value;
+    return this;
+  }
+
+  public build() {
+    return new Task(this);
   }
 }
+
 
 export enum ETaskPriority {
   VERYLOW = "Very Low",
