edb995057c0f14497ab926a0b9025c81428ef313
m0xai <keremzopcuk@gmail.com>
2023-09-30 22:18:37 +0200

    feat(frontend): create task from frontend app

--- web/src/app/features/task/components/task-create/task-create.component.ts
+++ web/src/app/features/task/components/task-create/task-create.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TaskService } from "../../services/task.service";
+import { ETaskPriority, ETaskStatus, Task } from "../../models/task.model";
 
 @Component({
   selector: 'app-task-create',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-create.component.css']
 })
 export class TaskCreateComponent {
+  @Input({required: true}) taskListId?: number;
+  newTaskNameInput = "";
 
+  constructor(private taskService: TaskService) {
+  }
+
+  addTaskToList() {
+    this.createTaskFromInput(this.newTaskNameInput);
+  }
+
+  private createTaskFromInput(input: string) {
+    if (input && this.taskListId) {
+      const newTask = Task.create().withName(input).withPriority(ETaskPriority.NORMAL).withStatus(ETaskStatus.OPEN).withTaskList(this.taskListId).build();
+      this.taskService.create(newTask).subscribe((value) => {
+        console.log("Task Created: ", value)
+      }, (error) => {
+        console.warn("Error oldu su an", error)
+      })
+    }
+  }
 }
