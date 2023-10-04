bf4f334a65e1a1168dd8f1082968036638ec220f
m0xai <keremzopcuk@gmail.com>
2023-10-04 18:38:56 +0200

    style(frontend): apply spacing with tabs to resource.service.ts

--- web/src/app/shared/resource.service.ts
+++ web/src/app/shared/resource.service.ts
@@ -4,45 +4,50 @@ import { map, Observable } from 'rxjs';
 import { ResourceModel } from './ResourceModel';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export abstract class ResourceService<T extends ResourceModel<T>> {
-  public apiURL = "";
-  private apiBase = "http://127.0.0.1:8080/api/v1/"
+    public apiURL = "";
+    private apiBase = "http://127.0.0.1:8080/api/v1/"
 
-  constructor(
-    public httpClient: HttpClient,
-    public tConstructor: { new(m: Partial<T>, ...args: unknown[]): T },
-    private apiPath: string
-  ) {
-    this.apiURL = this.apiBase + apiPath;
-  }
+    constructor(
+        public httpClient: HttpClient,
+        public tConstructor: { new(m: Partial<T>, ...args: unknown[]): T },
+        private apiPath: string
+    ) {
+        this.apiURL = this.apiBase + apiPath;
+    }
 
-  public create(resource: Partial<T> & { toJson: () => T }): Observable<T> {
-    return this.httpClient
-      .post<T>(`${this.apiURL}`, resource.toJson())
-      .pipe(map((result) => new this.tConstructor(result)));
-  }
+    public create(resource: Partial<T> & { toJson: () => T }): Observable<T> {
+        return this.httpClient
+            .post<T>(`${this.apiURL}`, resource.toJson())
+            .pipe(map((result) => new this.tConstructor(result)));
+    }
 
-  public fetch(): Observable<T[]> {
-    //? Headers only set for other requests, that require authentication
-    //   .set('withCredentials', 'true');
-    return this.httpClient
-      .get<T[]>(this.apiURL)
-      .pipe(map((result) => result.map((i) => new this.tConstructor(i))));
-  }
+    public fetch(): Observable<T[]> {
+        //? Headers only set for other requests, that require authentication
+        //   .set('withCredentials', 'true');
+        return this.httpClient
+            .get<T[]>(this.apiURL)
+            .pipe(map((result) => result.map((i) => new this.tConstructor(i))));
+    }
 
   public getById(id: number): Observable<T> {
     return this.httpClient
       .get<T>(`${this.apiURL}${id}/`)
       .pipe(map((result) => new this.tConstructor(result)));
   }
+    public getById(id: number): Observable<T> {
+        return this.httpClient
+            .get<T>(`${this.apiURL}${id}/`)
+            .pipe(map((result) => new this.tConstructor(result)));
+    }
 
-  public update(resource: Partial<T> & { toJson: () => T }): Observable<T> {
-    return this.httpClient
-      .put<T>(`${this.apiURL}${resource.id}/`, resource.toJson())
-      .pipe(map((result) => new this.tConstructor(result)));
-  }
+    public update(resource: Partial<T> & { toJson: () => T }): Observable<T> {
+        return this.httpClient
+            .put<T>(`${this.apiURL}${resource.id}/`, resource.toJson())
+            .pipe(map((result) => new this.tConstructor(result)));
+    }
 
   public delete(id: number): Observable<void> {
     return this.httpClient.delete<void>(`${this.apiURL}/${id}`);
