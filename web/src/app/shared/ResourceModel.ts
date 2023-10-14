b5f4f4fa55cb04736c0feb28c1c6542bba9aa957
m0xai <keremzopcuk@gmail.com>
2023-10-14 18:38:14 +0200

    feat(frontend): implement isEqual method for Resource Model

--- web/src/app/shared/ResourceModel.ts
+++ web/src/app/shared/ResourceModel.ts
@@ -11,6 +11,27 @@ export abstract class ResourceModel<T> implements IResource {
 		}
 	}
 
+	static isEqual(obj1: any, obj2: any) {
+		const props1 = Object.getOwnPropertyNames(obj1);
+		const props2 = Object.getOwnPropertyNames(obj2);
+		if (props1.length != props2.length) {
+			return false;
+		}
+		for (const element of props1) {
+			const val1: any = obj1[element];
+			const val2: any = obj2[element];
+			const isObjects = this.isObject(val1) && this.isObject(val2);
+			if ((isObjects && !this.isEqual(val1, val2)) || (!isObjects && val1 !== val2)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	static isObject(object: any) {
+		return object != null && typeof object === "object";
+	}
+
 	getId(): number {
 		return this.id!;
 	}
