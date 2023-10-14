f55a990bec16c5188f1d9bd838343509212daa72
m0xai <keremzopcuk@gmail.com>
2023-10-14 20:08:49 +0200

    feat(backend): add helper function to create ProjectDTOResponse from Project entity

--- backend/src/main/java/work/fertig/backend/project/dtos/ProjectDTOResponse.java
+++ backend/src/main/java/work/fertig/backend/project/dtos/ProjectDTOResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.stereotype.Component;
+import work.fertig.backend.project.Project;
 
 import java.sql.Timestamp;
 
@@ -18,7 +19,12 @@ public class ProjectDTOResponse implements ProjectDTO {
     private Boolean isArchived;
     private Timestamp createdAt;
     private Timestamp updatedAt;
+    private Long projectUsers;
 
     private ProjectDTOResponse() {
+    }
+
+    public static ProjectDTOResponse fromProject(Project project) {
+        return ProjectDTOResponse.builder().id(project.getId()).title(project.getTitle()).description(project.getDescription()).projectUsers(project.getProjectUsers().getId()).createdAt(project.getCreatedAt()).updatedAt(project.getUpdatedAt()).build();
     }
 }
