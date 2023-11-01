f9b0058619c74f3214484fbd0578fcaf3fbcd9c8
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-01 22:36:17 +0100

    feat(backend): create collaborator entity

---
+++ backend/src/main/java/work/fertig/backend/project/models/Collaborator.java
@@ -0,0 +1,40 @@
+package work.fertig.backend.project.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import work.fertig.backend.base.BaseEntity;
+import work.fertig.backend.user.FWUser;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "collaborator")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class Collaborator extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @OneToOne
+    @JoinColumn(name = "fw_user", referencedColumnName = "id")
+    private FWUser user;
+
+    @OneToOne
+    @JoinColumn(name = "project", referencedColumnName = "id")
+    private Project project;
+
+    private String role;
+    private Boolean isJoined;
+    private Timestamp joinedOn;
+    private Boolean isInvited;
+    private Timestamp invitedOn;
+
+    public Collaborator() {
+    }
+}
