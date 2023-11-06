c311e01ce45affa6a467cc00fa348d4e024836e6
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-06 23:45:50 +0100

    refactor(frontend): make Project variables public to access in template

--- web/src/app/features/project/models/project.model.ts
+++ web/src/app/features/project/models/project.model.ts
@@ -1,11 +1,11 @@
 import { ResourceModel } from "../../../shared/ResourceModel";
 
 export class Project extends ResourceModel<Project> {
-	private title?: string;
-	private description?: string;
-	private isArchived?: boolean;
-	private startOn?: Date;
-	private createdBy?: number;
+	public title?: string;
+	public description?: string;
+	public isArchived?: boolean;
+	public startOn?: Date;
+	public createdBy?: number;
 
 	constructor(model?: Partial<Project>) {
 		super(model);
