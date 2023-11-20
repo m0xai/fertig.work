fe769b458c3c01095583aaea67f9e4204c86a760
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-20 23:36:28 +0100

    feat(frontend): show projects, instead of static mock data

--- web/src/app/features/project/projects/projects.component.ts
+++ web/src/app/features/project/projects/projects.component.ts
@@ -1,8 +1,35 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ProjectResourceService } from "../services/project-resource.service";
+import { Project } from "../models/project.model";
+import { Router } from "@angular/router";
 
 @Component({
 	selector: "app-projects",
 	templateUrl: "./projects.component.html",
 	styleUrls: ["./projects.component.css"],
 })
-export class ProjectsComponent {}
+export class ProjectsComponent implements OnInit {
+	projectList: Project[] = [];
+
+	constructor(
+		private projectResourceService: ProjectResourceService,
+		private router: Router,
+	) {}
+
+	ngOnInit() {
+		this.projectResourceService.fetch().subscribe({
+			next: (value) => {
+				this.projectList = value;
+			},
+			error: (err) => {
+				console.log(err);
+			},
+		});
+	}
+
+	navigateToProject(projectId: number | undefined) {
+		if (projectId) {
+			this.router.navigateByUrl("app/projects/" + projectId);
+		}
+	}
+}
