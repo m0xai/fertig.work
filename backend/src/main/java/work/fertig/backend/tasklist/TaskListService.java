3791f6780cfba3260b53f4f66f688bcd054265b8
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-02-25 12:12:55 +0100

    refactor(backend): rewrite TaskService as interface and create implementation for it

--- backend/src/main/java/work/fertig/backend/tasklist/TaskListService.java
+++ backend/src/main/java/work/fertig/backend/tasklist/TaskListService.java
@@ -1,78 +1,21 @@
 package work.fertig.backend.tasklist;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-import work.fertig.backend.project.exceptions.ProjectNotFoundException;
-import work.fertig.backend.project.models.Project;
-import work.fertig.backend.project.services.ProjectService;
-import work.fertig.backend.task.TaskNotFoundException;
-import work.fertig.backend.task.TaskRepository;
 import work.fertig.backend.tasklist.dtos.TaskListDTORequest;
 import work.fertig.backend.tasklist.dtos.TaskListDTOResponse;
 import work.fertig.backend.tasklist.exceptions.TaskListNotFoundException;
 
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class TaskListService {
-    @Autowired
-    TaskListRepository taskListRepository;
-    @Autowired
-    TaskRepository taskRepository;
-    @Autowired
-    ProjectService projectService;
+public interface TaskListService {
+    TaskListDTOResponse create(TaskListDTORequest request);
 
-    public TaskListDTOResponse create(TaskListDTORequest request) {
-        try {
-            Project project = projectService.getEntity(request.getProject());
-            TaskList convertedTaskList = request.convertToEntity(project);
-            TaskList taskList = taskListRepository.save(convertedTaskList);
-            return TaskListDTOResponse.fromTaskList(taskList);
-        } catch (ProjectNotFoundException projectNotFoundException) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A Project with ID: " + request.getProject() + " " +
-                    "not found.");
-        }
-    }
+    List<TaskListDTOResponse> getAllByProject(Long projectId);
 
-    public List<TaskListDTOResponse> getAllByProject(Long projectId) {
-        if (projectService.getEntity(projectId) != null) {
-            return taskListRepository.getAllByProjectId(projectId)
-                    .stream()
-                    .map(TaskListDTOResponse::fromTaskList)
-                    .toList();
-        }
-        throw new ProjectNotFoundException("A project with ID: " + projectId + " not found");
-    }
+    TaskListDTOResponse get(Long id);
 
-    public TaskListDTOResponse get(Long id) {
-        try {
-            return TaskListDTOResponse.fromTaskList(this.getEntity(id));
-        } catch (TaskNotFoundException ex) {
-            throw new TaskListNotFoundException(id);
-        }
-    }
+    TaskList getEntity(Long id) throws TaskListNotFoundException;
 
-    public TaskList getEntity(Long id) throws TaskNotFoundException {
-        Optional<TaskList> oTaskList = taskListRepository.findById(id);
-        if (oTaskList.isEmpty()) {
-            throw new TaskListNotFoundException(id);
-        }
-        return oTaskList.get();
-    }
+    TaskListDTOResponse update(Long id, TaskListDTORequest request);
 
-    public TaskListDTOResponse update(Long id, TaskListDTORequest request) {
-        TaskList taskList = this.getEntity(id);
-        // NOTE: At this time, only title of the task list editable
-        taskList.setTitle(request.getTitle());
-        TaskList saved = taskListRepository.save(taskList);
-        return TaskListDTOResponse.fromTaskList(saved);
-    }
-
-    public void delete(Long id) throws TaskListNotFoundException {
-        var entity = this.getEntity(id);
-        this.taskListRepository.delete(entity);
-    }
+    void delete(Long id) throws TaskListNotFoundException;
 }
